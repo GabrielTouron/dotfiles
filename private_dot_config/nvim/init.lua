e5e0aa41b941e6c1031077f7463561eba61c6234
Gabriel Touron <gtcode@pm.me>
2023-01-06 16:17:49 +0100

    Add .config/nvim/.luarc.json Update .config/nvim/init.lua Add .config/nvim/plugin/packer_compiled.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -49,6 +49,7 @@ require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
 
   use 'navarasu/onedark.nvim' -- Theme inspired by Atom
+  use 'folke/tokyonight.nvim' -- An other theme
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
@@ -121,9 +122,50 @@ vim.o.smartcase = true
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
 
+vim.opt.relativenumber = true
+
+-- Moves lines and relesect in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- up
+
+-- Mode to the middle of the scren
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- down
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- up
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme onedark]]
+
+vim.o.cursorline = true
+
+require('onedark').setup {
+  style = 'deep',
+  highlights = {
+    CursorLineNr = { fg = "#FFD700" }
+  },
+}
+
+require('onedark').load()
+
+-- I don´t want neovim to ask me edit/read... about swapfile backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- No highlight after a search
+vim.opt.hlsearch = false
+
+-- Cursor will never be at the bottom of the window but 8 lines before
+vim.opt.scrolloff = 8
+
+
+-- vim.cmd [[colorscheme onedark]]
+-- vim.cmd [[colorscheme tokyonight]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -138,7 +180,6 @@ vim.g.maplocalleader = ' '
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
 vim.keymap.set('n', '<leader>pv', '<cmd>Ex<CR>', { silent = true })
 
 -- Remap for dealing with word wrap'
@@ -161,7 +202,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'onedark',
+    theme = 'tokyonight',
     component_separators = '|',
     section_separators = '',
   },
@@ -216,20 +257,22 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
+vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+
+vim.keymap.set('n', '<leader>fc', require('telescope.builtin').git_commits, { desc = '[F]ind [C]ommits' })
+vim.keymap.set('n', '<leader>cb', require('telescope.builtin').git_bcommits, { desc = '[C]ommit [B]uffers' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'typescript', 'help', 'vim' },
 
   highlight = { enable = true },
-  indent = { enable = true, disable = { 'python' } },
   incremental_selection = {
     enable = true,
     keymaps = {
@@ -330,6 +373,8 @@ local on_attach = function(_, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
+  nmap("<leader>ft", vim.lsp.buf.format, '[F]orma[T]')
+
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
@@ -347,7 +392,18 @@ local servers = {
   -- pyright = {},
   -- rust_analyzer = {},
   tsserver = {},
-
+  marksman = {},
+  robotframework_ls = {},
+  ansiblels = {},
+  astro = {},
+  eslint = {},
+  graphql = {},
+  html = {},
+  tailwindcss = {},
+  terraformls = {},
+  tflint = {},
+  yamlls = {},
+  bashls = {},
   sumneko_lua = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -406,7 +462,7 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.mapping.complete({}),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
